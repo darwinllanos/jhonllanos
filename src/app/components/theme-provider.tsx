515751650360268af369dc3f451184bf7a0b4155
darwinllanos <jhondarwinllanosnarvaez@gmail.com>
2025-04-13 21:17:53 -0500

    Correccion Error de Produccion

--- src/app/components/theme-provider.tsx
+++ src/app/components/theme-provider.tsx
@@ -1,11 +1,21 @@
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import React from 'react';
 
-export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
+
+export const ThemeProvider = ({ children, attribute = "class", defaultTheme = "system", enableSystem = true, disableTransitionOnChange = false}: ThemeProviderProps) => {
   return (
     <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem = {enableSystem}
+      disableTransitionOnChange={disableTransitionOnChange}
     >
       {children}
     </NextThemesProvider>

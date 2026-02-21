38e6eeb879b9666f8fe5d0d6682b8c7d71ef579f
darwinllanos <jhondarwinllanosnarvaez@gmail.com>
2026-02-21 12:51:21 -0500

    Modificacion de metriacas para rendimiento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import { Header } from "./components/header";
 import { Projects } from "./components/projects";
 import { Skills } from "./components/skills";
 import { Hero } from "./components/hero";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
+      <SpeedInsights />
       <Header />
       <main>
         <Hero />
